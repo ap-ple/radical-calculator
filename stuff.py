9b41e1a67360a6fca9403d5711895b81bcfa08bc
ap-ple <55882424+ap-ple@users.noreply.github.com>
2020-03-02 21:17:51 -0500

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,18 +1,18 @@
 import os
 
 #a shorter version of system('cls')
-def cls():
+def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 #Like input, but only accepts numbers; returns number in integer form, or 0 if the input is not a number
 def num_input(string=""):
     x = input(string)
-    return int(x) if x.isdigit() else 0
+    return int(x) if x.isdecimal() else 0
 
 #Like input, but clears after the input is taken
 def dialogue(string=""):
     x = input(string)
-    cls()
+    clear()
     return x
 
 #returns a string of the simplest radical form of the square root of a number
